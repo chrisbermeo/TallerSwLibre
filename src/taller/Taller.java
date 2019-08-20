c0193cd72ee70d78baeae203104ad1f992a8c670
chrisbermeo <chrisberme@gmail.com>
2019-08-20 14:47:19 -0500

    first commit

---
+++ src/taller/Taller.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP
+ */
+public class Taller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "HOla ");
+    }
+    
+}
